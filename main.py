2904f2614e1c27758ae264965875fb2688d7fd9e
jagernau <stepanenkov25@gmail.com>
2023-11-08 23:11:06 +0200

    привязка id контрагента к объекту через логин по id клиента, забыл активировать джоб

--- main.py
+++ main.py
@@ -57,167 +57,134 @@ logger.addHandler(file_handler)
 
 
 def job():
-    # try:
-          glonasssoft_data = merge_glonasssoft_data()
-    #     #add_objects(glonasssoft_data)
-          add_one_object(glonasssoft_data)
-    #     delete_one_object(glonasssoft_data)
-    #     update_one_object(glonasssoft_data)
-    #     logger.info("Глонасссофт успешно обновлен")
-    #
-    # except Exception as e:
-    #     logger.error(f"В обновлении Глонасссофтов возникла ошибка: {e}")
+    try:
+        glonasssoft_data = merge_glonasssoft_data()
+        #add_objects(glonasssoft_data)
+        add_one_object(glonasssoft_data)
+        delete_one_object(glonasssoft_data)
+        update_one_object(glonasssoft_data)
+        logger.info("Глонасссофт успешно обновлен")
+
+    except Exception as e:
+        logger.error(f"В обновлении Глонасссофтов возникла ошибка: {e}")
 
 
-    # try:
-    #     fort_data = merge_fort_data()
-    #     #add_objects(fort_data)
-    #     add_one_object(fort_data)
-    #     delete_one_object(fort_data)
-    #     update_one_object(fort_data)
-    #     logger.info("ФОРТ успешно обновлен")
-    # except Exception as e:
-    #     logger.error(f"В обновлении ФОРТ возникла ошибка: {e}")
+    try:
+        fort_data = merge_fort_data()
+        #add_objects(fort_data)
+        add_one_object(fort_data)
+        delete_one_object(fort_data)
+        update_one_object(fort_data)
+        logger.info("ФОРТ успешно обновлен")
+    except Exception as e:
+        logger.error(f"В обновлении ФОРТ возникла ошибка: {e}")
 
-    # try:
-    #     wialon_host_data = merge_wialon_host_data()
-    #     #add_objects(wialon_host_data)
-    #     add_one_object(wialon_host_data)
-    #     delete_one_object(wialon_host_data)
-    #     update_one_object(wialon_host_data)
-    #     logger.info("Вайлон хост успешно обновлен")
-    # except Exception as e:
-    #     logger.error(f"В обновлении Вайлон хоста возникла ошибка: {e}")
+    try:
+        wialon_host_data = merge_wialon_host_data()
+        #add_objects(wialon_host_data)
+        add_one_object(wialon_host_data)
+        delete_one_object(wialon_host_data)
+        update_one_object(wialon_host_data)
+        logger.info("Вайлон хост успешно обновлен")
+    except Exception as e:
+        logger.error(f"В обновлении Вайлон хоста возникла ошибка: {e}")
 
-    # try:
-    #     wialon_local_data = merge_wialon_local_data()
-    #     #add_objects(wialon_local_data)
-    #     add_one_object(wialon_local_data)
-    #     delete_one_object(wialon_local_data)
-    #     update_one_object(wialon_local_data)
-    #     logger.info("Вайлон локаль успешно обновлен")
-    # except Exception as e:
-    #     logger.error(f"В обновлении Вайлон локаль возникла ошибка: {e}")
+    try:
+        wialon_local_data = merge_wialon_local_data()
+        #add_objects(wialon_local_data)
+        add_one_object(wialon_local_data)
+        delete_one_object(wialon_local_data)
+        update_one_object(wialon_local_data)
+        logger.info("Вайлон локаль успешно обновлен")
+    except Exception as e:
+        logger.error(f"В обновлении Вайлон локаль возникла ошибка: {e}")
 
-    # try:
-    #     scout_data = merge_scout_data()
-    #     #add_objects(scout_data)
-    #     add_one_object(scout_data)
-    #     delete_one_object(scout_data)
-    #     update_one_object(scout_data)
-    #     logger.info("Скаут успешно обновлен")
-    # except Exception as e:
-    #     logger.error(f"В обновлении Скаута возникла ошибка: {e}")
+    try:
+        scout_data = merge_scout_data()
+        #add_objects(scout_data)
+        add_one_object(scout_data)
+        delete_one_object(scout_data)
+        update_one_object(scout_data)
+        logger.info("Скаут успешно обновлен")
+    except Exception as e:
+        logger.error(f"В обновлении Скаута возникла ошибка: {e}")
 
-    # try:
-    #     era_data = merge_era_data()
-    #     #add_objects(era_data)
-    #     add_one_object(era_data)
-    #     delete_one_object(era_data)
-    #     update_one_object(era_data)
-    #     logger.info("Эра успешно обновлен")
-    # except Exception as e:
-    #     logger.error(f"В обновлении Эра возникла ошибка: {e}")
-    #
-    # try:
-    #     glonass_clients = generate_glonass_client()
-    #     #add_sys_mon_clients(glonass_clients)
-    #     add_one_sys_mon_client(glonass_clients)
-    #     delete_one_sys_mon_client(glonass_clients)
-    #     update_one_sys_mon_client(glonass_clients)
-    #     logger.info("Хозяева Глонасс успешно обновлены")
-    # except Exception as e:
-    #     logger.error(f"В обновлении Хозяев Глонасс возникла ошибка: {e}")
-    #
-    # try:
-    #     fort_clients = generate_fort_companys()
-    #     #add_sys_mon_clients(fort_clients)
-    #     add_one_sys_mon_client(fort_clients)
-    #     delete_one_sys_mon_client(fort_clients)
-    #     update_one_sys_mon_client(fort_clients)
-    #     logger.info("Хозяева ФОРТ успешно обновлены")
-    # except Exception as e:
-    #     logger.error(f"В обновлении Хозяев ФОРТ возникла ошибка: {e}")
-    #
-    # try:
-    #     wialon_host_users = generate_wialon_host_users()
-    #     #add_sys_mon_clients(wialon_host_users)
-    #     add_one_sys_mon_client(wialon_host_users)
-    #     delete_one_sys_mon_client(wialon_host_users)
-    #     update_one_sys_mon_client(wialon_host_users)
-    #     logger.info("Хозяева Вайлон хост успешно обновлены")
-    # except Exception as e:
-    #     logger.error(f"В обновлении Хозяев Вайлон хост возникла ошибка: {e}")
-    #
-    # try:
-    #     wialon_local_users = generate_wialon_local_users()
-    #     #add_sys_mon_clients(wialon_local_users)
-    #     add_one_sys_mon_client(wialon_local_users)
-    #     delete_one_sys_mon_client(wialon_local_users)
-    #     update_one_sys_mon_client(wialon_local_users)
-    #     logger.info("Хозяева Вайлон локаль успешно обновлены")
-    # except Exception as e:
-    #     logger.error(f"В обновлении Хозяев Вайлон локаль возникла ошибка: {e}")
-    #
-    #
-    #     if computer_name != "max-SWH":
-    #
-    #         try:
-    #             clients_oneC = get_onec_clients()
-    #             # add_all_clients_oneC(clients_oneC)
-    #             add_one_oneC_clients(clients_oneC)
-    #             # delete_one_oneC_client(clients_oneC)
-    #             update_one_oneC_client(clients_oneC)
-    #             logger.info("Клиенты OneC успешно обновлены")
-    #         except Exception as e:
-    #             logger.error(f"В обновлении клиентов OneC возникла ошибка: {e}")
+    try:
+        era_data = merge_era_data()
+        #add_objects(era_data)
+        add_one_object(era_data)
+        delete_one_object(era_data)
+        update_one_object(era_data)
+        logger.info("Эра успешно обновлен")
+    except Exception as e:
+        logger.error(f"В обновлении Эра возникла ошибка: {e}")
+
+    try:
+        glonass_clients = generate_glonass_client()
+        #add_sys_mon_clients(glonass_clients)
+        add_one_sys_mon_client(glonass_clients)
+        delete_one_sys_mon_client(glonass_clients)
+        update_one_sys_mon_client(glonass_clients)
+        logger.info("Хозяева Глонасс успешно обновлены")
+    except Exception as e:
+        logger.error(f"В обновлении Хозяев Глонасс возникла ошибка: {e}")
+
+    try:
+        fort_clients = generate_fort_companys()
+        #add_sys_mon_clients(fort_clients)
+        add_one_sys_mon_client(fort_clients)
+        delete_one_sys_mon_client(fort_clients)
+        update_one_sys_mon_client(fort_clients)
+        logger.info("Хозяева ФОРТ успешно обновлены")
+    except Exception as e:
+        logger.error(f"В обновлении Хозяев ФОРТ возникла ошибка: {e}")
+
+    try:
+        wialon_host_users = generate_wialon_host_users()
+        #add_sys_mon_clients(wialon_host_users)
+        add_one_sys_mon_client(wialon_host_users)
+        delete_one_sys_mon_client(wialon_host_users)
+        update_one_sys_mon_client(wialon_host_users)
+        logger.info("Хозяева Вайлон хост успешно обновлены")
+    except Exception as e:
+        logger.error(f"В обновлении Хозяев Вайлон хост возникла ошибка: {e}")
+
+    try:
+        wialon_local_users = generate_wialon_local_users()
+        #add_sys_mon_clients(wialon_local_users)
+        add_one_sys_mon_client(wialon_local_users)
+        delete_one_sys_mon_client(wialon_local_users)
+        update_one_sys_mon_client(wialon_local_users)
+        logger.info("Хозяева Вайлон локаль успешно обновлены")
+    except Exception as e:
+        logger.error(f"В обновлении Хозяев Вайлон локаль возникла ошибка: {e}")
 
 
-# def execute_functions(functions):
-#     if not functions:
-#         return 
-#
-#     try:
-#         current_function = functions[0]
-#         current_function()
-#         logger.info(f"Выполнена функция {current_function.__name__}")
-#     except Exception as e:
-#         logger.error(f"Ошибка в функции {functions[0].__name__}: {e}")
-#         execute_functions(functions[1:])
-#     else:
-#         execute_functions(functions[1:])
-#             
-            
+        if computer_name != "max-SWH":
 
-# def job():
-#     functions = [
-#         glonass_object_data,
-#         fort_object_data,
-#         whost_object_data,
-#         wlocal_object_data,
-#         scout_object_data,
-#         era_object_data,
-#         glonass_clients_data,
-#         fort_clients_data,
-#         wialon_host_users_data,
-#         wialon_local_users_data,
-#         onec_clients_data
-#     ]
-#     execute_functions(functions)
-#         
+            try:
+                clients_oneC = get_onec_clients()
+                # add_all_clients_oneC(clients_oneC)
+                add_one_oneC_clients(clients_oneC)
+                # delete_one_oneC_client(clients_oneC)
+                update_one_oneC_client(clients_oneC)
+                logger.info("Клиенты OneC успешно обновлены")
+            except Exception as e:
+                logger.error(f"В обновлении клиентов OneC возникла ошибка: {e}")
 
-   
+
+  
 
 
 if __name__ == '__main__':
 
-    start_time = time.time()
-    job()
-    end_time = time.time()
-    execution_time = end_time - start_time
-    print(f"Execution time: {execution_time} seconds")
+    # start_time = time.time()
+    # job()
+    # end_time = time.time()
+    # execution_time = end_time - start_time
+    # print(f"Execution time: {execution_time} seconds")
 
-    # schedule.every().day.at("03:20").do(job)
-    # while True:
-    #     schedule.run_pending()
-    #     time.sleep(1)
+    schedule.every().day.at("03:20").do(job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
