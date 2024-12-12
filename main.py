00c1848584cc3bafff268b4a01d4c862f2eca70b
jagernau <stepanenkov25@gmail.com>
2024-12-12 12:55:22 +0300

    новый

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 # Создание обработчика для записи в файл
-file_handler = logging.FileHandler('new_new_log.txt')
+file_handler = logging.FileHandler('new_log.txt')
 file_handler.setLevel(logging.INFO)
 
 # Создание форматировщика
